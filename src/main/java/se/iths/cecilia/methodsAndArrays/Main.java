351171a7a64bb7e4b84fc28dc9168f313e4352a2
katttiger <katttiger@gmail.com>
2025-09-12 12:48:47 +0200

    Added descriptions to be printed, describing the methods and the tasks they are connected to. ONLY FOR CLARITY FOR USER.

--- src/main/java/se/iths/cecilia/methodsAndArrays/Main.java
+++ src/main/java/se/iths/cecilia/methodsAndArrays/Main.java
@@ -9,17 +9,19 @@ public class Main {
     public static void main(String[] args) {
         MyClass object = new MyClass();
 
+        System.out.println("----\nUppgift 1:" +
+                " Skriv ut värden i en String[]");
         String[] stringarray = {"Hello", "World", "!"};
         object.arrayToString(stringarray);
-        System.out.println("----");
 
+        System.out.println("----\nUppgift 2:" +
+                " Hitta det högsta talet i en int[].");
         int[] intArray = {111, 20, 345};
         System.out.println("Array: " + Arrays.toString(intArray));
         System.out.println(object.findMaxNumberInArray(intArray));
 
-        System.out.println("----");
-
-
+        System.out.println("----\nUppgift 3:" +
+                " Jämför två variabler av typen double och returnera det största värdet.");
         System.out.println("Enter first double: ");
         double a = sc.nextDouble();
         sc.nextLine();
@@ -29,7 +31,8 @@ public class Main {
         sc.nextLine();
         System.out.println("Highest number: " + object.findHighestNumber(a, b));
 
-        System.out.println("----");
+        System.out.println("----\nUppgift 4:" +
+                " Ta in två String-värden (för- och efternamn) och skriv ut dem som en mening.");
         System.out.println("Enter first name: ");
         String firstName = sc.nextLine();
         System.out.println("Enter first name: ");
